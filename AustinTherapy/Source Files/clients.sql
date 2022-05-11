088cf2cce0e0a0abfb20ea390de762a8030e3639
ArturoArriaga <32577651+ArturoArriaga@users.noreply.github.com>
2022-05-11 14:46:15 -0500

    Add files via upload

---
+++ AustinTherapy/Source Files/clients.sql
@@ -0,0 +1,239 @@
+
+#clients inserts
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (1,"Maxwell Navarro",4,3,1),
+  (4,"Zeus Banks",7,1,1),
+  (7,"Rose Espinoza",4,1,1),
+  (10,"Anthony Snyder",6,3,1),
+  (13,"Bevis Mullen",4,3,1),
+  (16,"Aladdin Keller",9,3,1),
+  (19,"Tatyana Farmer",8,2,1),
+  (22,"Kibo Franklin",5,2,1),
+  (25,"Cara Hill",8,2,1),
+  (28,"Irene Drake",9,1,1);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (31,"Hilary Blair",8,2,1),
+  (34,"Velma Chambers",4,1,1),
+  (37,"Angelica Arnold",6,2,1),
+  (40,"Wanda Cabrera",10,3,1),
+  (43,"Maryam Gentry",9,3,1),
+  (46,"Miranda Ware",4,2,1),
+  (49,"Cairo Foley",8,2,1),
+  (52,"Azalia Dickson",7,3,1),
+  (55,"Alyssa Mercado",10,1,1),
+  (58,"Ruth Brennan",9,2,1);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (61,"Castor Boone",6,2,1),
+  (64,"Shad Madden",5,2,1),
+  (67,"Chelsea Knox",3,1,1),
+  (70,"Hedley Duke",7,2,1),
+  (73,"Rafael Salazar",5,1,1),
+  (76,"Stewart Woodard",6,1,1),
+  (79,"Kelsey Lewis",9,2,1),
+  (82,"Portia Richmond",8,1,1),
+  (85,"Tana Cotton",8,3,1),
+  (88,"Scarlett Gallagher",6,3,1);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (91,"Brent Nicholson",3,3,1),
+  (94,"Nathaniel Howe",2,3,1),
+  (97,"Dora Valdez",9,3,1),
+  (100,"Hiram Pace",6,1,1),
+  (103,"Basil Daugherty",2,2,1),
+  (106,"Omar Hatfield",6,1,1),
+  (109,"Dawn Weiss",10,3,1),
+  (112,"Katell Benson",6,1,1),
+  (115,"Todd Oneil",9,3,1),
+  (118,"Angelica Frank",4,3,1);
+  
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (121,"Malcolm Hays",10,2,1),
+  (124,"John Downs",7,3,1),
+  (127,"Ulric Gamble",7,2,1),
+  (130,"Xanthus Barlow",5,2,1),
+  (133,"Jolene Malone",8,2,1),
+  (136,"Holmes Rodgers",8,2,1),
+  (139,"Clark Hurley",2,2,1),
+  (142,"Barclay Morrison",9,1,1),
+  (145,"Shea Henderson",9,3,1),
+  (148,"Dana Edwards",9,2,1);
+  
+  INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (2,"Cruz Tanner",13,5,1),
+  (5,"Darius Weeks",7,6,1),
+  (8,"Zenia Peters",6,3,1),
+  (11,"Jackson Vazquez",6,7,2),
+  (14,"Tyrone Cameron",13,6,2),
+  (17,"Zelenia Ray",11,5,2),
+  (20,"Nigel Morse",10,7,2),
+  (23,"Mallory Ball",11,6,1),
+  (26,"Christian Grimes",10,6,1),
+  (29,"Cherokee Hayden",13,7,2);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (32,"Xena Brock",11,8,1),
+  (35,"Connor Ramos",12,6,2),
+  (38,"Zena Wood",7,3,1),
+  (41,"Xyla Baird",10,3,1),
+  (44,"Michelle Hensley",10,1,1),
+  (47,"Bruno Ruiz",7,2,2),
+  (50,"Aquila Bartlett",12,4,2),
+  (53,"Seth Mcbride",8,2,2),
+  (56,"Nicholas James",4,8,2),
+  (59,"Asher Snow",11,7,1);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (62,"Brian Cabrera",2,5,2),
+  (65,"Porter Carney",12,4,1),
+  (68,"Prescott Hess",6,6,1),
+  (71,"Jenette Rose",5,6,1),
+  (74,"Tamekah Jarvis",7,8,2),
+  (77,"Celeste Charles",10,7,2),
+  (80,"Norman Eaton",12,2,1),
+  (83,"Geoffrey Olson",15,8,1),
+  (86,"Nora Miles",12,2,1),
+  (89,"Nyssa Matthews",12,7,1);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (92,"Jerome Mcintyre",10,6,2),
+  (95,"Chase Holloway",13,3,2),
+  (98,"Odysseus Anderson",7,8,2),
+  (101,"Ayanna Collins",10,7,2),
+  (104,"August Wooten",2,2,2),
+  (107,"Rhona Dillard",15,3,1),
+  (110,"Caleb Norton",5,3,1),
+  (113,"Erin Kemp",15,6,1),
+  (116,"Hedley Whitfield",11,6,2),
+  (119,"Gary Hodge",11,4,2);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (122,"Cheyenne Pollard",8,6,2),
+  (125,"Samuel Cooper",5,2,1),
+  (128,"Mark Zamora",7,2,1),
+  (131,"Jenna Pruitt",14,3,1),
+  (134,"Lionel Hensley",11,3,1),
+  (137,"Macaulay Talley",10,4,2),
+  (140,"Audrey Olson",4,7,1),
+  (143,"Lev Delgado",10,3,1),
+  (146,"Clinton Johns",6,7,1),
+  (149,"Len Howe",10,3,1);
+
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (3,"Fleur Bond",9,2,1),
+  (6,"Paula Mccray",6,3,3),
+  (9,"Josephine Dickson",8,3,2),
+  (12,"Barry Weiss",5,6,2),
+  (15,"Jason Alford",6,8,2),
+  (18,"Lars Bishop",6,6,3),
+  (21,"Tate Velazquez",6,6,2),
+  (24,"Pascale Rosario",9,6,1),
+  (27,"Palmer Oneal",9,4,2),
+  (30,"Theodore Sharpe",8,5,3);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (33,"Chantale Carney",7,3,2),
+  (36,"Mason Duncan",7,3,1),
+  (39,"Ivy Townsend",8,4,3),
+  (42,"Dominic Bates",9,2,3),
+  (45,"Iris Sears",9,1,1),
+  (48,"Ima Hammond",7,8,2),
+  (51,"Brianna Myers",9,5,2),
+  (54,"Brenden Gardner",10,3,1),
+  (57,"Berk Frost",5,3,2),
+  (60,"Linus Fischer",6,5,2);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (63,"Morgan Rodgers",6,7,2),
+  (66,"Priscilla Howe",9,1,3),
+  (69,"Declan Moon",5,3,1),
+  (72,"Sophia Mccall",7,5,1),
+  (75,"Hilel Sutton",7,4,2);
+  
+  INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (78,"Regina Bright",65,10,2),
+  (81,"Gay Pate",71,10,3),
+  (84,"Kamal Fry",56,10,1),
+  (87,"Carly Sutton",55,10,2),
+  (90,"Sandra Hopkins",70,10,4),
+  (93,"Martin Little",70,10,2),
+  (96,"Yael Martinez",66,10,1),
+  (99,"Amelia Walsh",68,10,3),
+  (102,"Ronan Spence",62,10,4),
+  (105,"Aurora Solis",65,10,3);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (108,"Arthur Horne",62,10,3),
+  (111,"Fritz Tucker",57,10,1),
+  (114,"Lillian Schwartz",71,10,4),
+  (117,"Carl Cortez",68,10,2),
+  (120,"Boris Mann",73,10,3),
+  (123,"Savannah Hunt",64,10,4),
+  (126,"Winifred Chambers",55,10,2),
+  (129,"Briar Massey",53,10,4),
+  (132,"Rinah Mercado",64,10,4),
+  (135,"Leah Villarreal",73,10,1);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (138,"Lionel Kirkland",65,10,2),
+  (141,"Ima Weeks",54,10,3),
+  (144,"Lee Daniel",70,10,3),
+  (147,"Wayne Shaw",60,10,3),
+  (150,"Imogene Ayers",66,10,2);
+
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (151,"Sade Velasquez",63,6,1),
+  (153,"Rama Cannon",50,6,1),
+  (155,"Isabella Bowers",58,6,1),
+  (157,"Samuel Montgomery",19,6,2),
+  (159,"Eric Cox",11,6,1),
+  (161,"Carter Sutton",37,6,1),
+  (163,"Simone Roth",46,6,1),
+  (165,"Glenna Suarez",49,6,1),
+  (167,"Seth Johnson",57,6,2),
+  (169,"Martin Talley",74,6,2);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (171,"Barclay Mcknight",45,6,1),
+  (173,"Noah Sellers",34,6,1),
+  (175,"Maggy Bender",55,6,2),
+  (177,"Angelica Spencer",23,6,2),
+  (179,"Daryl Shaw",16,6,2),
+  (181,"Ava Price",26,6,1),
+  (183,"Addison Fletcher",61,6,1),
+  (185,"Renee Price",6,6,1),
+  (187,"Tanner Gonzales",69,6,2),
+  (189,"Nicholas Alston",42,6,2);
+
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (152,"Latifah Love",31,9,3),
+  (154,"Colorado Bush",39,9,3),
+  (156,"Michael Meadows",49,9,1),
+  (158,"Roary Lindsay",48,9,3),
+  (160,"Kimberly Duncan",48,9,1),
+  (162,"Cyrus Montgomery",40,9,2),
+  (164,"Idona Bray",33,9,3),
+  (166,"Zelenia Graves",37,9,3),
+  (168,"Fletcher Lindsay",50,9,1),
+  (170,"Britanney Hewitt",38,9,2);
+INSERT INTO `clients` (`client_id`,`client_name`,`client_age`,`diagnosis_id`,`language_id`)
+VALUES
+  (172,"Adria Horne",40,9,2),
+  (174,"Bethany Cox",37,9,3),
+  (176,"Lunea Schultz",25,9,1),
+  (178,"Yvette Deleon",46,9,3),
+  (180,"Amela Ferrell",48,9,1),
+  (182,"Graham Mccormick",38,9,2),
+  (184,"Dorian Sandoval",29,9,2),
+  (186,"Sigourney Fernandez",32,9,3),
+  (188,"Xandra Doyle",27,9,2),
+  (190,"Tashya Nielsen",50,9,1);
+
